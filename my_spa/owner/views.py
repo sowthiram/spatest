ca034381eb630273c8effabfce40a182b20fc311
Vishnupk35 <109891344+Vishnupk35@users.noreply.github.com>
2022-12-14 13:26:34 +0530

    beautician category

--- my_spa/owner/views.py
+++ my_spa/owner/views.py
@@ -141,6 +141,10 @@ class AddBeauticiansView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["beauticians"]=Beautician.objects.all()
+
+        category=Beautician.objects.filter(category__category_name="Manicure")
+        print("Category is: ",category)
+        
         return context
 
     def post(self, request,*args,**kwargs):
