c598816f8f9cff16b54b7c5e1606673693f3f45c
shiflashareef <shiflachinnu246@gmail.com>
2022-12-15 16:28:30 +0530

    Package model

--- my_spa/owner/models.py
+++ my_spa/owner/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from customer.models import *
 class Memberships(models.Model):
     name = models.CharField(max_length=150,unique=True)
     validity = models.PositiveIntegerField()
@@ -10,3 +11,15 @@ class Memberships(models.Model):
 
     def __str__(self):
         return self.name
+
+class Package(models.Model):
+    name = models.CharField(max_length=150,unique=True)
+    price = models.PositiveIntegerField(null=True)
+    desc = models.CharField(max_length=200,null=True,blank=True)
+    image=models.ImageField(upload_to="images/",null=True)
+    status=models.BooleanField(default=True,null=True)
+    timeslot=models.ForeignKey(Timeslots,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
